56665129aeee45346e86457e60b1f9850b7f5e64
Businge Brian <busingebrian@gmail.com>
2025-01-16 18:11:44 +0300

    added the cross origin resources

--- salon_management.py
+++ salon_management.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from datetime import datetime
 
 # Initialize Flask app and database
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///salon.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+CORS(app)
 
 db = SQLAlchemy(app)
 
